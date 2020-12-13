5571fc3aa7ab48ba98c88fb33e626116724866b3
Ricardo Oliva Alonso <46902833+ricardoolivaalonso@users.noreply.github.com>
2020-12-12 23:38:18 -0600

    Add files via upload

---
+++ dev/js/main.js
@@ -0,0 +1,36 @@
+ScrollReveal().reveal( '.r-left', {
+    reset: true,
+    mobile: true,
+    duration: 1000,
+    delay: 100,
+    opacity: .0,
+    origin: 'left',
+    distance: '50px'
+});
+ScrollReveal().reveal( '.r-right', {
+    reset: true,
+    mobile: true,
+    duration: 1000,
+    delay: 110,
+    opacity: .0,
+    origin: 'right',
+    distance: '50px'
+});
+ScrollReveal().reveal( '.r-top', {
+    reset: true,
+    mobile: true,
+    duration: 1250,
+    delay: 250,
+    opacity: .0,
+    origin: 'bottom',
+    distance: '100px'
+});
+ScrollReveal().reveal( '.r-bottom', {
+    reset: true,
+    mobile: true,
+    duration: 1000,
+    delay: 250,
+    opacity: .0,
+    origin: 'top',
+    distance: '50px'
+});
